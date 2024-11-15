5a14480f76d3fdf5b1a2280543af50c06f77e6bc
Idayat Sanni <idayatsanni@gmail.com>
2024-11-15 11:23:20 -0500

    update player

--- templates/header.php
+++ templates/header.php
@@ -56,14 +56,14 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="../index.php">Home</a>
+    <a class="navbar-brand" href="\wnba\index.php">Home</a>
     <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="../admin/playerAdmin/playerAdmin.php">Player Admin</a>
+          <a class="nav-link" href="\wnba\admin\playerAd\playerAdmin.php ">Player Admin</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../admin/awardAdmin/awardAdmin.php">Award Admin</a>
+          <a class="nav-link" href="\wnba\admin\awardAd\awardAdmin.php">Award Admin</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="../logout.php">Log out</a>
